e8f02d14332790ccd581e702e46aad160c167d9f
Joe Jackson <ph8tel@gmail.com>
2018-04-23 13:31:43 -0700

    added logic to get comment replies

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
-var axios = require('axios')
+const axios = require('axios')
 
 module.exports = youtubeLogic = {
-    replyToComment: (parentId, commentText, accessToken, refresh_token, YID, YSEC) => {
+
+    replyToComment: (commentId, chanId, parentId, commentText, accessToken, refresh_token, YID, YSEC) => {
 
         return new Promise(resolve => {
             const google = require('googleapis')
@@ -24,33 +25,96 @@ module.exports = youtubeLogic = {
             let params = {
                 auth: oauth2Client,
                 part: "snippet",
-                snippet: {
-                    parentId: parentId,
-                    textOriginal: commentText
-                },
+                resource: {
+                    snippet: {
+                        channelId: chanId,
+                        topLevelComment: {
+                            snippet: {
+                                textOriginal: commentText
+                            }
+                        },
+                        videoId: parentId,
+                        commentId: commentId,
+                    }
+                }
             }
             youTubeDataApi.commentThreads.insert(params, (err, info) => {
                 if (err) {
                     console.log('Failure posting comment. This is how you messed up:', err.message);
-                    resolve(false);
+                    resolve("failed posting comment");
                 } else {
                     console.log('comment posted', info.statusText);
-                    resolve(info);
+                    resolve("posted comment");
                 }
             });
         })
     },
 
-    getPlaylists: function(playlistId, API_KEY) {
+    addComment: (chanId, parentId, commentText, accessToken, refresh_token, YID, YSEC) => {
+
+        return new Promise(resolve => {
+
+            const google = require('googleapis')
+            const youTubeDataApi = google.google.youtube('v3')
+            const OAuth2 = google.google.auth.OAuth2
+            const oauth2Client = new OAuth2(YID, YSEC, [])
+
+            // put the tokens in the header
+            oauth2Client.setCredentials({
+                refresh_token: refresh_token,
+                access_token: accessToken
+            });
+
+            //default set to tokens are in header
+            google.google.options({ auth: oauth2Client })
+
+            //build youtube commentResource object for request body
+            let params = {
+                auth: oauth2Client,
+                part: "snippet",
+                resource: {
+                    snippet: {
+                        channelId: chanId,
+                        videoId: parentId,
+                        topLevelComment: {
+                            snippet: {
+                                textOriginal: commentText
+                            }
+                        }
+                    }
+                }
+            }
+
+            youTubeDataApi.commentThreads.insert(params, (err, info) => {
+                if (err) {
+                    console.log('Failure posting comment. This is how you messed up:', err.message);
+                    resolve("failed posting comment");
+                } else {
+                    console.log('comment posted', info.statusText);
+                    resolve("posted comment");
+                }
+            })
+
+        })
+
+    },
+
+    getPlaylistItems: (chanID, API_KEY, token) => {
+
+        let params = {
+            playlistId: chanID,
+            maxResults: '50',
+            part: 'snippet,contentDetails',
+            key: API_KEY
+        }
+
+        if (token) {
+            params.pageToken = token
+        }
 
         return new Promise(resolve => {
             axios.get('https://www.googleapis.com/youtube/v3/playlistItems', {
-                params: {
-                    playlistId: playlistId,
-                    maxResults: '50',
-                    part: 'snippet,contentDetails',
-                    key: API_KEY
-                }
+                params: params
             }).then(playlists => {
                 let videoObjects = playlists.data.items.map(e => {
                     return {
@@ -61,13 +125,21 @@ module.exports = youtubeLogic = {
                         thumbnails: e.snippet.thumbnails
                     }
                 })
-                resolve(playlists.data.items)
-            }).catch(err => {
-                console.log('lists catch ran error', err.message)
-                console.error(err)
-                resolve([])
-            })
 
+                youtubeLogic.videoHolder = youtubeLogic.videoHolder.concat(videoObjects)
+
+                if (playlists.data.nextPageToken) {
+                    console.log('Found another page of videos, current count:', youtubeLogic.videoHolder.length)
+                    resolve(youtubeLogic.getPlaylistItems(chanID, API_KEY, playlists.data.nextPageToken))
+                } else {
+                    console.log('done getting all videos with', youtubeLogic.videoHolder.length)
+                    resolve(youtubeLogic.videoHolder)
+                }
+
+            }).catch(err => {
+                console.error('lists catch ran error', err.message)
+                resolve(err)
+            })
         })
     },
 
@@ -91,9 +163,8 @@ module.exports = youtubeLogic = {
             axios.get('https://www.googleapis.com/youtube/v3/playlistItems', {
                 params: params
             }).then(allVideos => {
-                console.log(allVideos.data.items[0], "SAMPLE")
                 let formattedVideos = allVideos.data.items.map(youtubeLogic.formattVideos)
-                youtubeLogic.videoHolder.concat(formattedVideos)
+                youtubeLogic.videoHolder = youtubeLogic.videoHolder.concat(formattedVideos)
 
                 if (allVideos.data.nextPageToken) {
                     resolve(youtubeLogic.getUploadedVideos(uploadsID, API_KEY, token))
@@ -130,50 +201,11 @@ module.exports = youtubeLogic = {
                     }
                 })
                 .then(deets => resolve(deets))
-                .catch(err => {
-                    console.log('error in get chan info,', err.message)
-                })
+                .catch(err => console.log('error in get chan infoerr', err.message))
         })
     },
-    gimmeVideos: async function(chanID, API_KEY) {
-        let uploadsId = 'UU' + chanID.slice(2)
-        let videos = await this.getUploadedVideos(uploadsId, API_KEY)
-        return videos
-    },
-    gimmeComments: async function(chanID, API_KEY) {
-        let commentObjects = await this.getComments(chanID, API_KEY)
-        return commentObjects
-    },
-    gimmePlaylist: async function(playlistId, API_KEY) {
-        let videoObjects = await this.getPlaylists(playlistId, API_KEY)
-        return videoObjects
-    },
-    gimmeAll: async function(userID, API_KEY) {
-        let channelInfo = await this.getChannelInfo(userID, API_KEY)
-        if (channelInfo.data.items.length) {
-            let uploadsID = channelInfo.data.items[0].contentDetails.relatedPlaylists.uploads;
-            let channelId = channelInfo.data.items[0].id
-            let commentObjects = await this.getComments(channelId, API_KEY)
-            let videoObjects = await this.getPlaylists(uploadsID, API_KEY)
 
-            let responseObject = {
-                videos: videoObjects,
-                comments: commentObjects,
-                uploadsPlaylist: uploadsID,
-                channelId: channelId,
-                userId: userID
-            }
-            return responseObject
-        } else {
-            return {
-                videos: [],
-                comments: [],
-                uploadsPlaylist: 'not found',
-                channelId: channelId,
-                userId: userID
-            }
-        }
-    },
+
 
     commentHolder: [],
 
@@ -251,7 +283,13 @@ module.exports = youtubeLogic = {
             axios.get('https://www.googleapis.com/youtube/v3/commentThreads', {
                 params: params
             }).then(async function(allComments) {
-
+                allComments.data.items.forEach(c => {
+                    if (c.replies) {
+                        console.log('found reply to comment')
+                        let replies = c.replies.comments.map(youtubeLogic.commentReplyFormatter)
+                        youtubeLogic.commentHolder = youtubeLogic.commentHolder.concat(replies)
+                    }
+                })
                 if (allComments.data.nextPageToken) {
 
                     youtubeLogic.commentHolder = youtubeLogic.commentHolder.concat(allComments.data.items.map(youtubeLogic.commentFormatter))
@@ -291,4 +329,102 @@ module.exports = youtubeLogic = {
         }
         return newFormat
     },
+    commentReplyFormatter: (e) => {
+        let comment = e.snippet.textDisplay
+        let newFormat = {
+            parentId: e.snippet.parentId,
+            author: e.snippet.authorDisplayName,
+            authorThumbnail: e.snippet.authorProfileImageUrl,
+            videoId: e.snippet.videoId,
+            comment: comment,
+            likeCount: e.snippet.likeCount,
+            publishedAt: e.snippet.publishedAt
+
+        }
+        if (comment.split('!').length > 1) {
+            newFormat.color = 'rgb(255, 99, 132)'
+        }
+        if (comment.split('?').length > 1) {
+            newFormat.color = 'rgb(54, 162, 235)'
+        }
+        return newFormat
+    },
+    getRepliesToComment: (commentId, API_KEY) => {
+        //returns an array of replies to a comment by id
+        return new Promise(resolve => {
+            axios.get('https://www.googleapis.com/youtube/v3/comments', {
+                    part: "snippet",
+                    maxResults: 50,
+                    parentId: commentId,
+                    key: API_KEY
+                })
+                .then(comments => {
+                    resolve(comments.data.items)
+                }).catch(err => {
+                    resolve(err.message)
+                })
+        })
+    },
+
+    getReplies: (commentId, API_KEY) => {
+        //returns an array of replies to a comment by id
+        return new Promise(resolve => {
+            axios.get('https://www.googleapis.com/youtube/v3/comments', {
+                    part: "snippet",
+                    maxResults: 50,
+                    parentId: commentId,
+                    key: API_KEY
+                })
+                .then(comments => {
+                    resolve(comments.data.items)
+                }).catch(err => {
+                    resolve(err.message)
+                })
+        })
+    },
+    gimmeVideos: async(chanID, API_KEY) => {
+        let uploadsID = 'UU' + chanID.slice(2)
+        let videos = await youtubeLogic.getPlaylistItems(uploadsID, API_KEY)
+
+        return videos
+    },
+
+    gimmeComments: async(chanID, API_KEY) => {
+        let commentObjects = await youtubeLogic.getComments(chanID, API_KEY)
+        return commentObjects
+    },
+
+    gimmePlaylist: async(uploadsID, API_KEY) => {
+        let videoObjects = await youtubeLogic.getPlaylists(uploadsID, API_KEY)
+        return videoObjects
+    },
+
+    gimmeAll: async function(userID, API_KEY) {
+        let channelInfo = await this.getChannelInfo(userID, API_KEY)
+        if (channelInfo.data.items.length) {
+
+            let uploadsID = channelInfo.data.items[0].contentDetails.relatedPlaylists.uploads;
+            let channelId = channelInfo.data.items[0].id
+
+            let commentObjects = await this.getComments(channelId, API_KEY)
+            let videoObjects = await this.getPlaylistItems(uploadsID, API_KEY)
+
+            let responseObject = {
+                videos: videoObjects,
+                comments: commentObjects
+            }
+
+            youtubeLogic.videoHolder = []
+            youtubeLogic.commentHolder = []
+
+            return responseObject
+
+        } else {
+
+            return {
+                videos: [],
+                comments: []
+            }
+        }
+    }
 }
